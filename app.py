9eb934b25ca2a731fa057cb2a0f3c7b780fa2deb
Juan Rivera <juanrivera.primary@gmail.com>
2021-10-24 12:47:02 -0400

    added JWT functionality

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ from flask_migrate import Migrate
 from flask_restful import Api
 
 from config import Config
-from extensions import db
+from extensions import db, jwt
 
-from resources.user import UserListResource
+from resources.user import UserListResource, UserResource, MeResource
+from resources.token import TokenResource
 from resources.recipe import RecipeListResource, RecipeResource, RecipePublishResource
 
 
@@ -23,6 +24,7 @@ def create_app():
 def register_extensions(app):
     db.init_app(app)
     migrate = Migrate(app, db)
+    jwt.init_app(app)
 
 
 def register_resources(app):
@@ -33,6 +35,9 @@ def register_resources(app):
     api.add_resource(RecipeResource, '/recipes/<int:recipe_id>')
     api.add_resource(RecipePublishResource, '/recipes/<int:recipe_id>/publish')
     api.add_resource(UserListResource, '/users')
+    api.add_resource(UserResource, '/users/<string:username>')
+    api.add_resource(TokenResource, '/token')
+    api.add_resource(MeResource, '/me')
 
 
 if __name__ == '__main__':
--- config.py
+++ config.py
@@ -8,6 +8,8 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 
 class Config:
     DEBUG = True
+    SECRET_KEY = 'super-secret-key'
+    JWT_ERROR_MESSAGE_KEY = 'message'
 
     SQLALCHEMY_DATABASE_URI = DATABASE_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
