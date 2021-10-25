403b50e5eb8593a8aa7e9d08cceebcc5cc6170c9
Juan Rivera <juanrivera.primary@gmail.com>
2021-10-24 22:44:11 -0400

    added image/avatar compression functionality

--- utils.py
+++ utils.py
@@ -1,8 +1,10 @@
 import uuid
+import os
 
 from flask_uploads import extension
 from extensions import image_set
 
+from PIL import Image
 from passlib.hash import pbkdf2_sha256
 
 
@@ -18,4 +20,34 @@ def save_image(image, folder):
     filename = '{}.{}'.format(uuid.uuid4(), extension(image.filename))
     image_set.save(image, folder=folder, name=filename)
 
+    filename = compress_image(filename=filename, folder=folder)
+
     return filename
+
+
+def compress_image(filename, folder):
+    file_path = image_set.path(filename=filename, folder=folder)
+
+    image = Image.open(file_path)
+
+    if image.mode != "RGB":
+        image = image.convert("RGB")
+
+    if max(image.width, image.height) > 1600:
+        maxsize = (1600, 1600)
+        image.thumbail(maxsize, Image.ANTIALIAS)
+
+    compressed_filename = '{}.jpg'.format(uuid.uuid4())
+    compressed_file_path = image_set.path(filename=compressed_filename, folder=folder)
+
+    image.save(compressed_file_path, optimize=True, quality=85)
+
+    original_size = os.stat(file_path).st_size
+    compressed_size = os.stat(compressed_file_path).st_size
+    percentage = round((original_size - compressed_size) / original_size * 100)
+
+    print('The file size is reduced by {}%, form {} to {}.'.format(percentage, original_size, compressed_size))
+
+    os.remove(file_path)
+
+    return compressed_filename
